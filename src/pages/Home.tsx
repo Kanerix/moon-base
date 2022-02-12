de685db7db32efe2e40294a4d58bb4b3c46c2cb9
Kanerix <dkkasjoe@hotmail.com>
2022-02-12 23:38:22 +0100

    Spelling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 					</div>
 					<div className='font-medium text-3xl'>Hvorfor?</div>
 					<div className='mb-4'>
-						Hvis os mennesker beslutter sig for på et tidpunkt at
+						Hvis os mennesker beslutter os for på et tidpunkt at
 						bygge en base på månen, skal der være en mulighed for at
 						vi kan transportere os selv rundt på månen. Hvis du
 						nogen sinde har tænkt på hvordan det kommer til at
@@ -31,10 +31,11 @@ export default function Home() {
 					</div>
 					<div className='font-medium text-3xl'>Hvordan?</div>
 					<div className='mb-4'>
-						Månebilen fungere ved brug af hover moduler. Dette er
-						fordi er det på månen næsten ingen tyngde kræft er, og
-						der er derfor ekstremt nemt at skubbe ting væk fra
-						månen.
+						Månebilen fungere lidt ligesom et hover board. Dette er
+						fordi er der på månen næsten ingen tyngdekræft er, det
+						er derfor ekstremt nemt at skubbe ting væk fra månen og
+						det gør det muligt at transpotere tunge ting meget nemt
+						og hurtigt, uden at bruge for mange rescourcer.
 					</div>
 					<div className='font-medium text-3xl'>Model</div>
 					<div className='mb-2'>
