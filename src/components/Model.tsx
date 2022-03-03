2409e0934dfa7e55ebdbaa4684d56e1649f1178a
Kanerix <dkkasjoe@hotmail.com>
2022-03-03 12:28:20 +0100

    Fixed cybertruck stuff

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,7 +1,17 @@
 import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Canvas, useLoader } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import Car from './Car'
+
+function Cybertruck() {
+	const gltf = useLoader(GLTFLoader, '/models/cybertruck.gltf')
+
+	return (
+		<Suspense fallback={null}>
+			<primitive scale={2} object={gltf.scene} />
+		</Suspense>
+	)
+}
 
 export default function Model() {
 	return (
@@ -9,7 +19,7 @@ export default function Model() {
 			<Suspense fallback={null}>
 				<ambientLight intensity={0.2} />
 				<pointLight position={[0, 10, 0]} intensity={0.1} />
-				<Car scale={0.1} />
+				<Cybertruck />
 				<OrbitControls
 					enablePan={false}
 					enableZoom={false}
